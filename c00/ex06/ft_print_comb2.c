b03c9a7a026ece5c3148c1abe6bb96ed95d58699
Kylian Aubry <kyaubry@c2r11p2.42lehavre.fr>
2023-03-02 16:57:32 +0100

    c00

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyaubry <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 20:57:18 by kyaubry           #+#    #+#             */
+/*   Updated: 2023/03/02 16:27:13 by kyaubry          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	call_ft_putchar(int a, int b)
+{
+	ft_putchar(a / 10 + '0');
+	ft_putchar(a % 10 + '0');
+	ft_putchar(' ');
+	ft_putchar(b / 10 + '0');
+	ft_putchar(b % 10 + '0');
+	if (a != 98)
+	{
+		write (1, &", ", 2);
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			call_ft_putchar(a, b);
+			b++;
+		}
+		a++;
+	}
+}
