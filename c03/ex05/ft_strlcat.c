e8787b8b3c02f973015542c5ee1f8f3666ebe777
Kylian Aubry <kyaubry@c2r10p2.42lehavre.fr>
2023-03-07 01:40:58 +0100

    c03

---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyaubry <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/07 01:30:49 by kyaubry           #+#    #+#             */
+/*   Updated: 2023/03/07 01:36:40 by kyaubry          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <string.h>
+
+unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
+{
+
+}
+
+int	main(void)
+{
+	char	dest[];
+	char	src[];
+
+	ft_strlcat();
+	return (0);
+}
